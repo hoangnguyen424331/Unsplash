cc350baa39b5bf2f462f6a9f14c8ea3bb5da99a1
hoangnguyen424331 <kingstyle424331@gmail.com>
2021-07-16 16:32:54 +0700

    Ui Search

--- app/src/main/java/com/example/unsplash/ui/search/search_photo/SearchPhotoFragment.kt
+++ app/src/main/java/com/example/unsplash/ui/search/search_photo/SearchPhotoFragment.kt
@@ -1,4 +1,28 @@
 package com.example.unsplash.ui.search.search_photo
 
-class SearchPhotoFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.unsplash.databinding.FragmentSearchPhotoBinding
+
+class SearchPhotoFragment : Fragment() {
+
+    lateinit var binding: FragmentSearchPhotoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchPhotoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    fun searchPhoto(keyWord: String) {}
+
+    companion object {
+        fun newInstance() = SearchPhotoFragment()
+    }
 }
---
+++ app/src/main/java/com/example/unsplash/utils/TypeSearch.kt
@@ -0,0 +1,6 @@
+package com.example.unsplash.utils
+
+enum class TypeSearch(name: String) {
+    PHOTO("Photo"),
+    COLLECTION("Collection")
+}
