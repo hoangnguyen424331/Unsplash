4699fa46c5fcfe51cb515a614bee36fa9c57956a
hoangnguyen424331 <kingstyle424331@gmail.com>
2021-07-16 14:52:03 +0700

    Create Database

---
+++ app/src/main/java/com/example/unsplash/data/source/local/sqlite/dao/ImageDao.kt
@@ -0,0 +1,23 @@
+package com.example.unsplash.data.source.local.sqlite.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.unsplash.data.source.local.sqlite.entity.ImageLocal
+
+@Dao
+interface ImageDao {
+
+    @Query("SELECT * FROM imageLocal")
+    suspend fun getImages(): List<ImageLocal>
+
+    @Insert
+    suspend fun insertImages(imageLocal: ImageLocal)
+
+    @Delete
+    suspend fun deleteImages(imageLocal: ImageLocal)
+
+    @Query("SELECT * FROM imageLocal WHERE id LIKE :imageId")
+    suspend fun getImage(imageId: String?): ImageLocal
+}
