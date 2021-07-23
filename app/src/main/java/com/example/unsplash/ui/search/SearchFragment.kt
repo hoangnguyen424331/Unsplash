ea88804e3c336ce4cb1fbbc7321a05d96f0bef57
hoangnguyen424331 <kingstyle424331@gmail.com>
2021-07-23 18:45:36 +0700

    Lưu và hiển thị lịch sử tìm kiếm

--- app/src/main/java/com/example/unsplash/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/unsplash/ui/search/SearchFragment.kt
@@ -1,12 +1,19 @@
 package com.example.unsplash.ui.search
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import android.content.SearchRecentSuggestionsProvider.DATABASE_MODE_QUERIES
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.SearchRecentSuggestions
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.unsplash.BuildConfig.APPLICATION_ID
 import com.example.unsplash.databinding.FragmentSearchBinding
 import com.example.unsplash.ui.search.adapter.SearchAdapter
 import com.example.unsplash.ui.search.search_collection.SearchCollectionFragment
@@ -15,6 +22,8 @@ import com.example.unsplash.ui.search.search_photo.SearchPhotoFragment
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
+    private lateinit var searchManager: SearchManager
+    private lateinit var suggestions: SearchRecentSuggestions
     private val searchAdapter by lazy { SearchAdapter(childFragmentManager) }
 
     override fun onCreateView(
@@ -28,9 +37,31 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        registerService()
         initTab()
         handEvent()
         binding()
+    }
+
+    private fun registerService() {
+        suggestions = SearchRecentSuggestions(
+            requireContext(),
+            APPLICATION_ID,
+            DATABASE_MODE_QUERIES
+        )
+        searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        binding.searchView.setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
+        requireActivity().intent.apply {
+            if (Intent.ACTION_SEARCH == action) {
+                getStringExtra(SearchManager.QUERY)?.also {
+                    SearchRecentSuggestions(
+                        requireContext(),
+                        APPLICATION_ID,
+                        DATABASE_MODE_QUERIES
+                    ).saveRecentQuery(it, null)
+                }
+            }
+        }
     }
 
     private fun initTab() {
@@ -56,16 +87,37 @@ class SearchFragment : Fragment() {
                 query?.let {
                     searchAdapter.setKeyword(it)
                 }
+                suggestions.saveRecentQuery(query, null)
                 binding.searchView.clearFocus()
                 return false
             }
 
             override fun onQueryTextChange(newText: String?) = false
         })
+        binding.searchView.setOnSuggestionListener(object : SearchView.OnSuggestionListener {
+
+            override fun onSuggestionSelect(position: Int) = true
+
+            override fun onSuggestionClick(position: Int): Boolean {
+                binding.searchView.setQuery(getQuerySearchView(position), true)
+                return true
+            }
+        })
+    }
+
+    private fun getQuerySearchView(position: Int): String {
+        val selectedView = binding.searchView.suggestionsAdapter
+        val cursor = selectedView.getItem(position) as Cursor
+        val index = cursor.getColumnIndexOrThrow(SearchManager.SUGGEST_COLUMN_TEXT_1)
+        return cursor.getString(index).toString()
     }
 
     private fun binding() {
-        binding.lifecycleOwner = this
+        binding.run {
+            lifecycleOwner = this@SearchFragment
+            searchView.isFocusable = false
+            searchView.requestFocusFromTouch()
+        }
     }
 
     companion object {
